2942794e086314b93e0e2c1f81892ba17f7fdf5b
Alan Mabry <alan.mabry@gmail.com>
2016-09-08 14:35:08 -0700

    404

--- app/router.js
+++ app/router.js
@@ -8,6 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function () {
   this.route('about');
   this.route('artist', {path: '/artist/:artist_name'});
+  this.route('404', {
+    path: '/*path'
+  });
 });
 
 export default Router;
