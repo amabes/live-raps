96e8479207a3da108b6f408ceab648864ebc54b5
Alan Mabry <alan.mabry@gmail.com>
2016-09-08 14:34:48 -0700

    404

---
+++ tests/unit/routes/404-test.js
@@ -0,0 +1,12 @@
+import {moduleFor, test} from 'ember-qunit';
+
+moduleFor('route:404', 'Unit | Route | 404', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function (assert) {
+  const route = this.subject();
+
+  assert.ok(route);
+});
